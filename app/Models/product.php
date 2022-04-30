9628e7a93748d9c8274354badfffe1d47ed9a87c
HamasakiBrain <herovuwbdal@gmail.com>
2022-04-30 12:59:12 +0300

    Create Migrations, Model and Controller

--- app/Models/product.php
+++ app/Models/product.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class product extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'code', 'name', 'level1', 'level2', 'level3',
+        'price', 'priceCP', 'count', 'properties',
+        'joint', 'unit', 'image', 'showMain',
+        'description'
+    ];
 }
--- database/migrations/2022_04_30_094716_create_products_table.php
+++ database/migrations/2022_04_30_094716_create_products_table.php
@@ -15,6 +15,20 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('code');
+            $table->string('name');
+            $table->string('level1')->nullable();
+            $table->string('level2')->nullable();
+            $table->string('level3')->nullable();
+            $table->string('price')->nullable();
+            $table->string('priceCP')->nullable();
+            $table->string('count')->nullable();
+            $table->string('properties')->nullable();
+            $table->string('joint')->nullable();
+            $table->string('unit')->nullable();
+            $table->string('image')->nullable();
+            $table->string('showMain')->nullable();
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
